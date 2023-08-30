d7016fcc2a4820b1f7cd665502765ee1fbc3fd30
unknown <esaums@gmail.com>
2023-08-30 00:53:26 -0600

    terminado el backend

--- api/src/controllers/dietasControler.js
+++ api/src/controllers/dietasControler.js
@@ -3,14 +3,33 @@ const axios = require("axios");
 const { API_KEY } = process.env;
 
 const getAllDiets = async() => {
+    
     const allDiets = [];
-    const infoFromApi = (await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)).data.results;
-    allDiets.push(...Object.keys(infoFromApi[0]).slice(0,4));
-    infoFromApi.map((recipe)=>{
-        allDiets.push(...recipe.diets);
-    })    
+    //dbCount para verificar si la DB se encuentra vacía.
+    const dbCount = await Diet.count();
+
+    if(dbCount ===0){
+        // const infoFromApi = (await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)).data.results;
+        const infoFromApi = (await axios.get(`https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5`)).data.results;
+
+        allDiets.push(...Object.keys(infoFromApi[0]).slice(0,4));
+        infoFromApi.map((recipe)=>{
+            allDiets.push(...recipe.diets);
+        })    
+    } else {
+        const dbDiets = await Diet.findAll();
+        dbDiets.map((diet)=>{
+            allDiets.push(diet.name)
+        })
+    }
     const filterDiets = [...new Set(allDiets)];
-    return filterDiets;
+    const arrayOfDiets = filterDiets.map((diet)=>{
+        return{ name: diet}
+    })
+
+    const createDietsDB = await Diet.bulkCreate(arrayOfDiets);
+    const dbDiets = await Diet.findAll();
+    return dbDiets;
 }
 
 
