5fc1546cf142406663c21f0f181056cb143b2f78
unknown <esaums@gmail.com>
2023-08-29 23:55:53 -0600

    obtuve el array de dietas de la api

--- api/src/controllers/dietasControler.js
+++ api/src/controllers/dietasControler.js
@@ -3,8 +3,14 @@ const axios = require("axios");
 const { API_KEY } = process.env;
 
 const getAllDiets = async() => {
-    const algo = "esau";
-    return algo;
+    const allDiets = [];
+    const infoFromApi = (await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)).data.results;
+    allDiets.push(...Object.keys(infoFromApi[0]).slice(0,4));
+    infoFromApi.map((recipe)=>{
+        allDiets.push(...recipe.diets);
+    })    
+    const filterDiets = [...new Set(allDiets)];
+    return filterDiets;
 }
 
 
